b6c0f025adbf12a8ebf0ec371b6c84d595a55ac3
JC <834096423@qq.com>
2017-05-10 22:57:09 +0800

    DataStructure Expand

--- unit14/IntervalTree.py
+++ unit14/IntervalTree.py
@@ -1,18 +1,19 @@
 #coding:utf-8
 import random
 import copy
-class RBTreeNode:
-    def __init__(self,key = None,color = None,size = 1):
+class IntervalTreeNode:
+    def __init__(self,key = None,color = None,interval = None):
         self.key = key
         self.color = color
         self.p = None
         self.left = None
         self.right = None
-        self.size = size
+        self.interval = interval
+        self.max = max(interval)
 
-class RBTree:
+class IntervalTree:
     def __init__(self):
-        self.Nil = RBTreeNode(None,"Black",0)
+        self.Nil = IntervalTreeNode(None,"Black",(-float('inf'),-float('inf')))
         self.Root = self.Root.p = self.Nil
     def TreeMin(self,Node):
         MinNode = Node
@@ -22,30 +23,21 @@ class RBTree:
     def OrderPrintTree(self,Tree,i):
         if Tree != self.Nil:
             self.OrderPrintTree(Tree.left,i+1)
-            print '\t' * i + str(Tree.key) + ":"  + Tree.color + ":" + str(Tree.size)
+            print '\t' * i + str(Tree.interval) + "-" + str(Tree.max) + ":"  + Tree.color
             self.OrderPrintTree(Tree.right,i+1)
-    def Select(self,Tree,i):
-        rank = Tree.left.size + 1
-        if i == rank:
-            return Tree
-        elif i < rank:
-            return self.Select(Tree.left,i)
-        else:
-            return self.Select(Tree.right,i - rank)
-    def Rank(self,Node):
-        rank = Node.left.size + 1
-        
-        NodeTemp = Node
-        while NodeTemp != self.Root:
-            if NodeTemp.p.right == NodeTemp:
-                rank = rank + NodeTemp.p.left.size + 1
-            NodeTemp = NodeTemp.p
-        return rank
-        
+    def IntervalSearch(self,TimeNode):
+        IsIntervalOverlap = lambda x,y: True if x[0] <= y[1] and y[0] <= x[1] else False
+        x = self.Root
+        while x != self.Nil and not IsIntervalOverlap(TimeNode,x.interval):
+            if x.left != self.Nil and x.left.max >= TimeNode[0]:
+                x = x.left
+            else:
+                x = x.right
+        return x
             
     def LeftRotate(self,Node):
         RightSon = Node.right           #将Node右子树的左子树作为Node的新的右子树
-        Node.right = RightSon.left 
+        Node.right = RightSon.left
         if RightSon.left != self.Nil:   
             RightSon.left.p = Node
         RightSon.p = Node.p             #用Node的右子树代替Node，进行Node父节点的拼接
@@ -57,10 +49,7 @@ class RBTree:
             Node.p.right = RightSon
         RightSon.left = Node           #用Node作为其右子树的左节点，完成拼接
         Node.p = RightSon
-        
-        RightSon.size = Node.size
-        Node.size = Node.left.size + Node.right.size + 1
-        
+        RightSon.max = max(RightSon.interval[1],RightSon.left.max,RightSon.right.max) #更新最大区间，选择原始区间最大值与子树的区间最大值比较
     def RightRotate(self,Node):
         LeftSon = Node.left            #用Node的左子树的右孩子作为Node的左子树
         Node.left = LeftSon.right
@@ -77,16 +66,16 @@ class RBTree:
         
         LeftSon.right = Node         #用Node作为其左子树的右节点，完成拼接
         Node.p = LeftSon
-        LeftSon.size = Node.size
-        Node.size = Node.left.size + Node.right.size + 1
-        
-        
-        
+        LeftSon.max = max(LeftSon.interval[1],LeftSon.left.max,LeftSon.right.max) #更新最大区间，选择原始区间最大值与子树的区间最大值比较
+    def UpdateNodeMax(self,Node):
+        ParentNode = Node.p
+        while ParentNode != self.Nil:
+            ParentNode.max = max(ParentNode.interval[1],ParentNode.left.max,ParentNode.right.max)
+            ParentNode = ParentNode.p
     def RB_Insert(self,Node):
         ParentNode = self.Nil
         x = self.Root
         while x != self.Nil:
-            x.size = x.size + 1          #沿着路径size +　１
             ParentNode = x
             if Node.key < x.key:
                 x = x.left
@@ -102,7 +91,9 @@ class RBTree:
         Node.left = self.Nil
         Node.right = self.Nil
         Node.color = "Red"
+        self.UpdateNodeMax(Node)  #顺子节点向上更新其父节点的max值
         self.RB_InsertFixUp(Node)
+        
     def RB_InsertFixUp(self,Node):
         while Node.p.color == "Red" :
             if Node.p == Node.p.p.left:        #父亲节点为爷爷节点的左节点
@@ -142,29 +133,19 @@ class RBTree:
             OldNode.p.left = NewNode
         else:
             OldNode.p.right = NewNode
-        NewNode.size = OldNode.size
         NewNode.p = OldNode.p
-    def ReSizeFromNodeToRoot(self,Node):
-        ParentNode = Node.p 
-        while ParentNode != self.Nil: 
-            ParentNode.size = ParentNode.size - 1
-            ParentNode = ParentNode.p
-        
     def RB_Delete(self,Node):
         y = Node  
         yOrginalColor = y.color
         if Node.left == self.Nil:
             x = Node.right
-            self.ReSizeFromNodeToRoot(y)
             self.RB_TransPlant(Node,Node.right)
         elif Node.right == self.Nil:
             x = Node.left
-            self.ReSizeFromNodeToRoot(y)
             self.RB_TransPlant(Node,Node.left)
         else:
             y = self.TreeMin(Node.right)
             yOrginalColor = y.color
-            self.ReSizeFromNodeToRoot(y)
             x = y.right
             if y.p == Node:
                 x.p = y
@@ -177,6 +158,7 @@ class RBTree:
             y.left.p = y
             y.color = Node.color
         
+        self.UpdateNodeMax(x)    
         if yOrginalColor == "Black":
             self.RB_DeleteFixUp(x)
     def RB_DeleteFixUp(self,Node):
@@ -227,29 +209,25 @@ class RBTree:
                     Node = self.Root
         Node.color = "Black"
                 
-RBTreeob = RBTree()
-list = [26,17,41,14,21,30,47,10,16,19,21,28,38,7,12,14,20,35,39,3]
+IntervalTreeob = IntervalTree()
+list = [ (16,21),(8,9),(25,30),(5,8),(15,23),(17,19),(26,26),(0,3),(6,10),(19,20) ]
+
 TreeNode = [0] * len(list)
 
+
 for i in range(len(list)):
-    TreeNode[i] = RBTreeNode(list[i])
-    RBTreeob.RB_Insert(TreeNode[i])
-
-
-# for i in range(100):
-    # TreeNode[i] = RBTreeNode(random.randint(1,100))
-    # RBTreeob.RB_Insert(TreeNode[i])
+    TreeNode[i] = IntervalTreeNode(list[i][0],None,list[i])
+    IntervalTreeob.RB_Insert(TreeNode[i])
 
 
 
-# for i in range(50):
-    # RBTreeob.RB_Delete(TreeNode[i])
+#IntervalTreeob.RB_Delete(TreeNode[8])
+
     
 
 
 
-RBTreeob.OrderPrintTree(RBTreeob.Root,0)
-print RBTreeob.Select(RBTreeob.Root,17).key
-print RBTreeob.Rank(TreeNode[12])
-
+IntervalTreeob.OrderPrintTree(IntervalTreeob.Root,0)
+print u"查找(22,25)的重叠区间: ",
+print IntervalTreeob.IntervalSearch((22,25)).interval
 
