ad989e6731b0585f237b7574defd5795de64c508
JC <834096423@qq.com>
2017-06-07 22:28:08 +0800

    DisjointSets

--- 21_Data Structures for Disjoint Sets/DisjointSets.py
+++ 21_Data Structures for Disjoint Sets/DisjointSets.py
@@ -2,8 +2,7 @@ class NodeSet:
     def __init__(self,key):
         self.parent = self
         self.rank = 0
-        self.key = []
-        self.key.append(key)
+        self.keys = key
 class DisjointSets:
     def __init__(self):
         pass
@@ -12,14 +11,14 @@ class DisjointSets:
     def _link(self,set1,set2):
         if set1.rank > set2.rank:
             set2.parent = set1
-            set1.key.extend(set2.key)
-            return set1
+            set1.keys = set1.keys + ',' + set2.keys
+            return set2
         else:
             set1.parent = set2
-            set2.key.extend(set1.key)
+            set2.keys = set2.keys + ',' + set1.keys
             if set1.rank == set2.rank:
                 set2.rank = set2.rank + 1
-            return set2
+            return set1
     def union(self,set1,set2):
         return self._link(self.find_set(set1),self.find_set(set2))
     def find_set(self,set):
@@ -32,12 +31,17 @@ Vertex = ['a','b','c','d','e','f','g','h','i','j']
 Edge = [ ('a','b'),('a','c'),('b','a'),('b','c'),('b','d'),('c','a'),('c','b'),
          ('d','b'),('e','f'),('e','g'),('f','e'),('g','e'),('h','i'),('i','h') ]
          
-SetDir = {}
-DisjointSetsList = []
-for V in Vertex:
-    SetDir[V] = DisjointSetsOb.make_set(V)
-for edge in Edge:
-    if DisjointSetsOb.find_set(SetDir[edge[0]]) != DisjointSetsOb.find_set(SetDir[edge[1]]):
-        NodeSet = DisjointSetsOb.union(SetDir[edge[0]],SetDir[edge[1]])
+SetDirQuery = {}
+SetDirResult = {}
 
-    
+for V in Vertex:
+    disjointSet = DisjointSetsOb.make_set(V)
+    SetDirQuery[V] = disjointSet
+    SetDirResult[V] = disjointSet
+for edge in Edge:
+    if DisjointSetsOb.find_set(SetDirQuery[edge[0]]) != DisjointSetsOb.find_set(SetDirQuery[edge[1]]):
+        DelNodeSet = DisjointSetsOb.union(SetDirQuery[edge[0]],SetDirQuery[edge[1]])
+        del SetDirResult[DelNodeSet.keys]
+
+for key,set in SetDirResult.iteritems():
+    print set.keys
